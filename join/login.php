d739b64954f716a9fe757fdc4f936e20a39b2604
aki <a.test.acount2020jp@gmail.com>
2020-10-25 20:18:41 +0900

    [edit]chenge

--- join/login.php
+++ join/login.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css" media="all">
+    <link rel="stylesheet" type="text/css" href="../style.css" media="all">
     <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&display=swap" rel="stylesheet">
     <title>My Recipe Collection ～私のレシピ集～</title>
@@ -14,25 +14,61 @@
 <div id="wrapper">
 
     <header class="clearfix">
-
         <h1>My Recipe Collection</h1>
         
     </header>
-    <div id="topimage">
+
+    <div id="topimage" class="clearfix">
             <p>自分のレシピを記録しよう!</p>
 
-            <img src="../images/silverware.png" alt="テーブルウェア" height="80px" width="120px">
+            <img src="silverware.png" alt="テーブルウェア" height="70px" width="100px">
     </div>
 
-    <div id="container">
+    <div id="container" class="clearfix">
+
         <div id="centerpart">
-           
+
+            <div id="leftside">
+                <p>メールアドレスとパスワードを記入して</p>
+                <p>ログインしてください。</p>
+                <br><br>
+                <p>会員登録をされてない方はこちらから</p>
+                <div class="box2">
+                    <p><a href="#">会員登録する</a></p>
+                </div>
+            </div>
+            <div id="rightside">
+                <form action="" method="post">
+                    <d1>
+                        <dt>メールアドレス</dt>
+                        <dd>
+                            <input type="text" name="email" size="35" maxlength="255" value="#" />
+                            
+                        </dd>
+                        <dt>パスワード</dt>
+                        <dd>
+                            <input type="password" name="password" size="35" maxlength="255" value="#" />
+                        </dd>
+                        <dt>次回から自動ログインする</dt>
+                        <dd>
+                            <input id="save" type="checkbox" name="save" value="on">
+                            <label for="save">ON</label>
+                        </dd>
+                    </d1>
+                    <div>
+                        <input type="submit" value="ログイン" />
+                    </div>
+                </form>
+            </div>
+
         </div><!--centerpart end-->
     </div><!--countainer end-->
-
 </div><!--wrapper end-->
 <footer>
+    <a href="../index.html">戻る</a>
+    <br><br>
     <p><small>© 2020 ay.</small></p>
+
 </body>
 
 </html>
